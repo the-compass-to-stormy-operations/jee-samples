c214515872733775426388518c04cc785e245955
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2011-04-03 17:40:23 -0300

    teste mb

---
+++ exemplo-webapp/src/main/java/br/gov/serpro/exemplo/webapp/view/managedbean/TesteMB.java
@@ -0,0 +1,15 @@
+package br.gov.serpro.exemplo.webapp.view.managedbean;
+
+public class TesteMB {
+	
+	private String field;
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+}
