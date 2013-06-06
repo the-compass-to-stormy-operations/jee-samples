770cc6c1cdd859de85172f8e167dce58575308e7
Heitor Rapcinski <heitor.rapcinski@gmail.com>
2013-06-06 09:39:06 -0300

    :# Please enter the commit message for your changes. Lines starting

--- jee6-exemplo1-web/src/main/java/tirando/onda/jee/web/faces/Login.java
+++ jee6-exemplo1-web/src/main/java/tirando/onda/jee/web/faces/Login.java
@@ -1,12 +1,16 @@
 package tirando.onda.jee.web.faces;
 
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
 
 @ManagedBean(name="login")
 @RequestScoped
@@ -49,22 +53,44 @@ public class Login {
 		this.x509 = x509;
 	}
 
-	public void autenticateUsernamePassword() {
+	public String autenticateUsernamePassword() {
 		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", username);
+		map.put("password", password);
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(map);
+
+		System.out.println(json);
+		
 		try {
-			request.login(username, password);
+			request.login("USERNAME-PASSWORD", json);
 		} catch (ServletException e) {
-			throw new RuntimeException("Error in programmatic login", e);
+			throw new RuntimeException("Failed login with Username/Password", e);
 		}	
+		return "";
 	}
 	
-	public void logout() {
+	public String autenticateClientCertificate() {
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		try {
+			request.login("CLIENT-CERTIFICATE", x509.toString());
+		} catch (ServletException e) {
+			throw new RuntimeException("Failed login with Client Certificate", e);
+		}	
+		return "";
+	}
+	
+	public String logout() {
 		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
 		try {
 			request.logout();
 		} catch (ServletException e) {
-			throw new RuntimeException("Error in programmatic logout", e);
+			throw new RuntimeException("Failed logout", e);
 		}
+		return "";
 	}
 
 }
